eb5959bae26624bb2559f2b85b779e94ffd56005
Hatzilu <itamar246@gmail.com>
2024-11-19 19:53:03 +0200

    outline

--- src/components/ViewPort.tsx
+++ src/components/ViewPort.tsx
@@ -3,6 +3,7 @@ import { createSharpTexture } from '@/utils/three.utils';
 import React, { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { EffectComposer, OutlinePass, RenderPass } from 'three/examples/jsm/Addons.js';
 
 const ViewPort = () => {
 
@@ -11,11 +12,35 @@ const ViewPort = () => {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
 
+    const outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), scene, camera);
     const renderer = new THREE.WebGLRenderer();
+
+    const raycaster = new THREE.Raycaster();
+    const pointer = new THREE.Vector2();
+
+    const renderPass = new RenderPass(scene, camera);
+    const clock = new THREE.Clock();
+
+    let selectedObjects = [];
 
     useEffect(() => {
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        renderer.shadowMap.enabled = true;
+
+
+        outlinePass.visibleEdgeColor.set("#ffffff");
+        outlinePass.hiddenEdgeColor.set('#000000');
+        outlinePass.edgeStrength = 5;
+        outlinePass.edgeGlow = 0;
+        outlinePass.edgeThickness = 1;
+        outlinePass.pulsePeriod = 0;
+
+        renderPass.setSize(window.innerWidth, window.innerHeight)
+        renderPass.renderToScreen = true;
+        const effectComposer = new EffectComposer(renderer)
+        effectComposer.addPass(renderPass);
+        effectComposer.addPass(outlinePass);
 
         const light2 = new THREE.DirectionalLight(0xffffff, 1);
         light2.position.set(0, 1, 0);
@@ -26,9 +51,6 @@ const ViewPort = () => {
         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         const cube = new THREE.Mesh(geometry, material);
         // scene.add(cube);
-
-
-
 
         const light = new THREE.AmbientLight(0x404040); // soft white light scene.add( light );
         scene.add(light);
@@ -71,13 +93,49 @@ const ViewPort = () => {
 
         const controls = new OrbitControls(camera, renderer.domElement);
 
+        function onPointerClick(event: MouseEvent) {
+            pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+            pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+            console.log(pointer);
+
+            let objs = scene.children.filter(obj => obj.type === 'Mesh' && obj.name === 'Grass block')
+            const intersects = raycaster.intersectObjects(objs);
+            console.log({ intersects, objs })
+            if (!intersects.length) {
+                selectedObjects = [];
+                outlinePass.selectedObjects = selectedObjects;
+                return;
+            }
+
+            for (let i = 0; i < intersects.length; i++) {
+                let m = intersects[i].object;
+                if (m.name === "Grass block") {
+                    selectedObjects = [];
+                    selectedObjects.push(m);
+                    outlinePass.selectedObjects = selectedObjects;
+                }
+            }
+        }
+
         function animate() {
             cube.rotation.x += 0.01; cube.rotation.y += 0.01;
+            const delta = clock.getDelta()
             // mesh.rotation.y += 0.01;
             // mesh.rotation.x += 0.01;
             controls.update()
-            renderer.render(scene, camera);
+            raycaster.setFromCamera(pointer, camera);
+            // renderer.render(scene, camera);
+            effectComposer.render(delta)
         };
+
+        function onWindowResize() {
+
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize(window.innerWidth, window.innerHeight);
+
+        }
 
         renderer.setAnimationLoop(animate);
 
@@ -85,8 +143,15 @@ const ViewPort = () => {
         if (!c) {
             return;
         }
+
+        c.addEventListener('click', onPointerClick);
+        window.addEventListener('resize', onWindowResize)
         c.appendChild(renderer.domElement);
 
+        return () => {
+            c.removeEventListener('click', onPointerClick)
+            window.removeEventListener('resize', onWindowResize)
+        }
     }, [])
 
     const addGrass = () => {
@@ -103,10 +168,12 @@ const ViewPort = () => {
         ];
 
         let mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
-        mesh.position.y = Math.random() * 5;
+        mesh.position.y = -4.5;
         mesh.position.x = Math.random() * 5;
         mesh.position.z = Math.random() * 5;
         mesh.receiveShadow = true;
+        mesh.castShadow = true;
+        mesh.name = "Grass block";
         scene.add(mesh)
     }
 
